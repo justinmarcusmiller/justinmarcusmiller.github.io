673fe581a1daf76583aee00ccb38c3d667b536fb
justinmarcusmiller <jmiller056@student.butte.edu>
2014-12-09 13:36:48 -0800

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,30 @@
+ <div class="nav">
+ 	<div class="name">
+ Justin Miller
+ <br>
+ 	<div class="sitename">
+ 	Justinmarcusmiller.com
+	</div>
+ <br>
+ </div>
+ <br>
+ 
+<a href="index.html">Home</a><br><br><br><br>
+
+<a href="games.html">Games</a><br><br>
+
+<a href="apps.html">Apps</a><br><br>
+
+<a href="pics.html">Photos</a><br><br>
+
+<a href="resources.html">Resources</a><br><br><br><br>
+
+
+
+<a href="about.html">About Me</a><br><br>
+
+<a href="contact.html">Contact Me</a><br><br><br>
+
+<a href="http://steamcommunity.com/id/RadioactiveTuna/">Steam</a><br><br>
+
+</div>
