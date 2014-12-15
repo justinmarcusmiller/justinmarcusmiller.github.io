f5cc160fb85ed2a79225cffe1836706baf511c4b
justinmarcusmiller <jmiller056@student.butte.edu>
2014-12-14 16:46:47 -0800

    Update and rename games.html to games.php

---
+++ games.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="en-us" http-equiv="Content-Language">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="styles.css">
+    <style type="text/css">
+	.auto-style1 {
+		margin-left: 0;
+		margin: 15 15 15 0px;
+		clear: right;
+		float: left;
+		padding-left: 0;
+	}
+	</style>
+  </head>
+
+  <body>
+ 
+ <?php include("nav.php"); ?>
+
+
+<div class="main">
+	<div class="heading">
+	<p>Games</p>
+	</div>
+These are some games I have made using <a href="http://unity3d.com/">Unity</a> and Java.
+</div>
+  
