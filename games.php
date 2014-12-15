aa373ef59f8d908f0ba6cefe519855d23ec9c596
justinmarcusmiller <jmiller056@student.butte.edu>
2014-12-14 16:57:56 -0800

    Update games.php

--- games.php
+++ games.php
@@ -17,7 +17,30 @@
 
   <body>
  
- <?php include("nav.php"); ?>
+<div class="nav">
+ 	<div class="name">
+ 	Justin Miller
+	 
+ 		<div class="sitename">
+ 		Justinmarcusmiller.com
+ 		<br><br>
+		</div>
+	</div>
+<br>
+<br>
+ 
+<a href="index.html">Home</a><br><br>
+<a href="about.html">About Me</a><br><br><br><br>
+<a href="games.html">Games</a><br><br>
+<a href="apps.html">Apps</a><br><br>
+<a href="pics.html">Photos</a><br><br>
+<a href="websites.html">Websites</a><br><br><br><br>
+
+<a href="mailto:Justinmiller171@gmail.com">E-Mail</a><br><br>
+<a href="https://github.com/justinmarcusmiller">Github</a><br><br>
+<a href="http://steamcommunity.com/id/RadioactiveTuna/">Steam</a><br><br>
+<a href="https://twitter.com/JMarcusMiller">Twitter</a><br><br>
+</div>
 
 
 <div class="main">
