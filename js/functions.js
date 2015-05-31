1350cf9dee6d483d70ba2108352c5cc7bc8cd880
justinmarcusmiller <jmiller056@student.butte.edu>
2015-05-31 03:15:01 -0700

    Update

--- js/functions.js
+++ js/functions.js
@@ -3,20 +3,8 @@ $(window).scroll(function(){
 /*Header Parallax*/
   var wScroll = $(this).scrollTop();
 
-  $('header h1').css({
+  $('.logo').css({
     'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-  });
-
-$('.sun').css({
-    'transform' : 'translate(0px, '+ wScroll /3 +'%)'
-  });
-    
-  $('.m2').css({
-    'transform' : 'translate(0px, '+ wScroll /40 +'%)'
-  });
-
-  $('.m1').css({
-    'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
   });
     
 });
