c38b2571f9e1fa360c49026a797964f8b0ba2a5a
justinmarcusmiller <jmiller056@student.butte.edu>
2015-06-03 02:41:11 -0700

    update

--- js/functions.js
+++ js/functions.js
@@ -3,8 +3,5 @@ $(window).scroll(function(){
 /*Header Parallax*/
   var wScroll = $(this).scrollTop();
 
-  $('.logo').css({
-    'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-  });
-    
+
 });
