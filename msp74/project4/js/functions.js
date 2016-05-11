17f8cfb98662d917c27db1e37305d255e7152426
justinmarcusmiller <jmiller056@student.butte.edu>
2016-05-11 03:57:30 -0700

    Revert "Update Project 4"
    This reverts commit d997987642b1089a2660cbebfcd9bf185bb3f28e.

--- msp74/project4/js/functions.js
+++ msp74/project4/js/functions.js
@@ -1,22 +1,9 @@
 $(document).ready(function () {
     "use strict";
     
-    $(window).scroll(function () {
-        
-        var wScroll = $(this).scrollTop();
-        
-        if (wScroll > $('.main-bottom-sections').offset().top) {
-            
-            $('.main-cta-nav').addClass('is-fixed');
-            
-        };
-        
-        if (wScroll < $('.main-bottom-sections').offset().top) {
-            
-            $('.main-cta-nav').removeClass('is-fixed');
-            
-        };
+    $('*').hide();
+    $('*').fadeIn(1000);
 
-    });
+    
 });
 
