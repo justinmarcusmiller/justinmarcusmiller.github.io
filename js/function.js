fe989e6801c726e4d6751ee36dd2583bbfcc9239
Justin Miller <jmiller056@student.butte.edu>
2019-09-07 04:49:00 -0700

    New Design

--- js/function.js
+++ js/function.js
@@ -1,18 +1,3 @@
 $(document).ready(function(){
 
-    // Moves name left and right when scrolling down
-    $(window).scroll(function() {
-        let distance = $(window).scrollTop();
-        $('#first-name').css({ 'transform': 'translate(' + -distance + 'px, 0px)'});
-        $('#last-name').css({ 'transform': 'translate(' + distance + 'px, 0px)'});
-
-    });
-
-    // moves view to about section when spash button is clicked/pressed
-    $("#splashbutton").click(function() {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#about").offset().top
-        }, 1000);
-    });
-
 });
